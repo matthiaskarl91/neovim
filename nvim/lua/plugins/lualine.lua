d890c446d4c38981fa31acafe28299dc95dcd509
Matthias Karl <matthias.karl@exaring.de>
2025-08-28 10:26:48 +0200

    wipo

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,5 +1,7 @@
-vim.pack.add({"https://github.com/nvim-lualine/lualine.nvim"})
+vim.pack.add({ "https://github.com/nvim-lualine/lualine.nvim" })
 
 require("lualine").setup({
-
+  options = {
+    theme = 'dracula'
+  }
 })
