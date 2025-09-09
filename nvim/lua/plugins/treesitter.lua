45c264894bf32ebef62078b569c78ca49b74eaf3
Matthias Karl <matthias.karl@exaring.de>
2025-09-09 16:05:37 +0200

    treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,29 +2,32 @@ vim.pack.add({
   { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" }
 })
 
-require("nvim-treesitter.configs").setup({
-  ensure_installed = {
-    'lua',
-    'luadoc',
-    'luap',
-    'go',
-    'gomod',
-    'gowork',
-    'gosum',
-    'graphql',
-    'tsx',
-    'typescript',
-    'vim',
-    'vimdoc',
-
-  },
-  auto_install = false,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-  indent = {
-    enable = true,
-  },
-  parser_install_dir = vim.fn.stdpath('data') .. '/treesitter'
+require("nvim-treesitter").setup({
+  install_dir = vim.fn.stdpath('data') .. '/treesitter'
 })
+
+--require("nvim-treesitter").setup({
+--  ensure_installed = {
+--   'lua',
+-- 'luadoc',
+-- 'luap',
+--'go',
+-- 'gomod',
+--  'gowork',
+--  'gosum',
+--  'graphql',
+--  'tsx',
+--  'typescript',
+--  'vim',
+--  'vimdoc',
+-- },
+-- auto_install = false,
+-- highlight = {
+--   enable = true,
+--   additional_vim_regex_highlighting = false,
+-- },
+-- indent = {
+--   enable = true,
+-- },
+-- install_dir = vim.fn.stdpath('data') .. '/treesitter'
+--})
