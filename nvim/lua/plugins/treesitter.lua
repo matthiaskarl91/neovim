315e9cb08fffc5721e171fe8a697b8081ec36469
Matthias Karl <matthias.karl@exaring.de>
2025-09-10 09:52:03 +0200

    chore: revert treesitter to master

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,9 @@
 vim.pack.add({
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" }
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "master" }
 })
 
 require("nvim-treesitter").setup({
-  install = {
+  ensure_installed = {
     'lua',
     'luadoc',
     'luap',
@@ -17,34 +17,13 @@ require("nvim-treesitter").setup({
     'vim',
     'vimdoc',
   },
+  auto_install = false,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  indent = {
+    enable = true,
+  },
   install_dir = vim.fn.stdpath('data') .. '/treesitter'
 })
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'typescriptreact' },
-  callback = function() vim.treesitter.start() end,
-})
---require("nvim-treesitter").setup({
---  ensure_installed = {
---   'lua',
--- 'luadoc',
--- 'luap',
---'go',
--- 'gomod',
---  'gowork',
---  'gosum',
---  'graphql',
---  'tsx',
---  'typescript',
---  'vim',
---  'vimdoc',
--- },
--- auto_install = false,
--- highlight = {
---   enable = true,
---   additional_vim_regex_highlighting = false,
--- },
--- indent = {
---   enable = true,
--- },
--- install_dir = vim.fn.stdpath('data') .. '/treesitter'
---})
