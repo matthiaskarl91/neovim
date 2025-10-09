99718d60cd4f84e94286be54748c10a63f9825a8
Matthias Karl <matthias.karl@exaring.de>
2025-10-09 15:24:39 +0200

    wip

--- nvim/lua/plugins/nvim-lint.lua
+++ nvim/lua/plugins/nvim-lint.lua
@@ -6,12 +6,28 @@ local golangcilint = require("lint").linters.golangcilint
 golangcilint.ignore_exitcode = true
 
 
+--[[lint.linters_by_ft = {
+  --lua = { "luacheck" },
+  python = { "flake8" },
+  javascript = { "eslint_d" },
+  typescript = { 'eslint_d' },
+  typescriptreact = { 'eslint_d' },
+  json = { "jsonlint" },
+  yaml = { "yamllint" },
+  proto = { "buf_lint" },
+  nix = { "statix" },
+  go = { "golangcilint" },
+  sh = { "shellcheck" },
+  bash = { "shellcheck" },
+  zsh = { "shellcheck" },
+  fish = { "shellcheck" },
+}]]
 lint.linters_by_ft = {
   --lua = { "luacheck" },
   python = { "flake8" },
-  javascript = { "eslint" },
-  typescript = { 'eslint' },
-  typescriptreact = { 'eslint' },
+  javascript = { "eslint_d" },
+  typescript = { 'eslint_d' },
+  typescriptreact = { 'eslint_d' },
   json = { "jsonlint" },
   yaml = { "yamllint" },
   proto = { "buf_lint" },
@@ -23,6 +39,19 @@ lint.linters_by_ft = {
   fish = { "shellcheck" },
 }
 
+local eslint = lint.linters.eslint_d
+
+eslint.args = {
+  "--no-warn-ignored", -- <-- this is the key argument
+  "--format",
+  "json",
+  "--stdin",
+  "--stdin-filename",
+  function()
+    return vim.api.nvim_buf_get_name(0)
+  end,
+}
+
 local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
 vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufEnter', 'InsertLeave' }, {
@@ -30,6 +59,23 @@ vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufEnter', 'InsertLeave' }, {
   callback = function()
     local linters = lint.linters_by_ft[vim.bo.filetype]
     if linters and #linters > 0 then
+      -- check if eslint config exists
+      if vim.bo.filetype == "javascript" or vim.bo.filetype == "typescript" then
+        local eslintConfigFilenames = {
+          "eslint.config.js",
+          "eslint.config.mjs",
+          "eslint.config.cjs",
+          "eslint.config.ts",
+          "eslint.config.mts",
+          "eslint.config.cts",
+          ".eslintrc.js",
+          ".eslintrc.json",
+          ".eslintrc.cjs",
+        }
+        if vim.fs.root(0, eslintConfigFilenames) == nil then
+          return
+        end
+      end
       lint.try_lint()
     end
   end
