ad42debc83b8ba1778d64d28bb2d79ac1f9feaf5
Matthias Karl <matthias.karl@exaring.de>
2025-12-08 11:39:24 +0100

    wip

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,2 +1,5 @@
 vim.pack.add({ "http://github.com/folke/tokyonight.nvim.git" })
 vim.pack.add({ "https://github.com/p00f/alabaster.nvim" })
+
+vim.cmd [[colorscheme tokyonight]]
+--vim.cmd [[colorscheme alabaster]]
